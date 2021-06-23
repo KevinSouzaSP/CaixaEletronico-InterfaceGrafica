e1eacb393989a93e96f13c504578247d1dc7cd40
KevinSouzaSP <kevin.guilherme@outlook.com>
2021-06-23 15:30:08 -0300

    Primeiro commit

---
+++ src/caixaeletronicoui/CaixaeletronicoUI.java
@@ -0,0 +1,11 @@
+
+package caixaeletronicoui;
+
+public class CaixaeletronicoUI {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/caixaeletronicoui/ExemploMessageDialog.java
@@ -0,0 +1,16 @@
+
+package caixaeletronicoui;
+
+import caixaeletronicoui.ExemploMessageDialogUI;
+
+public class ExemploMessageDialog {
+
+    public static void main(String[] args) {
+        
+        ExemploMessageDialogUI objForm = new ExemploMessageDialogUI();
+        
+        objForm.show();
+        
+    }
+    
+}
